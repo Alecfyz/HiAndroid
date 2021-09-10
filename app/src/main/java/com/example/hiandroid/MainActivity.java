0327517096db1debb016ae171d95e88d65f373de
Alex <alecfyz@gmail.com>
2021-09-11 02:31:47 +0300

    Basic commit.

--- app/src/main/java/com/example/hiandroid/MainActivity.java
+++ app/src/main/java/com/example/hiandroid/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.hiandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView greetingsTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        greetingsTv = findViewById(R.id.greetings_text_view);
+
+        greetingsTv.setText("Hi there! \nI`m a first view.");
     }
+
 }
